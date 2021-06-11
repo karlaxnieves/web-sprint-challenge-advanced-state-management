d9b54a00c0a74281eb0514145c7b86c0f2cda672
Karla Nieves <karlaxnieves@gmail.com>
2021-06-11 07:47:09 -0700

    working on adding a smurf

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import {error, fetchFail, addSmurf} from '../actions'
 
 const AddForm = (props) => {
+const {error, fetchFail, addSmurf} = props;
+
     const [state, setState] = useState({
         name:"",
         position:"",
@@ -17,9 +21,8 @@ const AddForm = (props) => {
 
     const handleSubmit = e => {
         e.preventDefault();
-        if (state.name === "" || state.position === "" || state.nickname === "") {
-            errorMessage = "Name, position and nickname fields are required.";
-        }
+        addSmurf(state);
+        error();
     }
 
     const errorMessage = "";
@@ -51,7 +54,7 @@ const AddForm = (props) => {
     </section>);
 }
 
-export default AddForm;
+export default connect(null, {error, fetchFail, addSmurf})(AddForm);
 
 //Task List:
 //1. Connect the errorMessage, setError and addSmurf actions to the AddForm component.
